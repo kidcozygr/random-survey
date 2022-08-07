167467c272f36896e34b2c8d0b3e7953fb57470b
kidcozygr <110769286+kidcozygr@users.noreply.github.com>
2022-08-07 20:02:49 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,10 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://docs.google.com/forms/d/1h34V-fRoTsXUDnvJBY3J4khuxGDMfRgpquWZzQpKDNA/edit#responses',
+	'https://docs.google.com/forms/d/130-0pEFDje34CeNuyuUa2xgkP6NbJaVesx_-A3uwW4E/edit',
+	'https://docs.google.com/forms/d/1tJcdrUCZC1Fsovb4Enjz8_HTvtKatuDk-w89bSro2dU/edit',
+	'https://docs.google.com/forms/d/1xhozidzehbZh-JE1RJ938GFxQ3NBKYMVtUJi975iJWs/edit',
+	'https://docs.google.com/forms/d/1SwsJTF7c3SrdURvYUUjvTslapxX1gm0z31Aaz_VPLZg/edit',
+	'https://docs.google.com/forms/d/1TGGM2P8rJJ6r490kFmSkavh1a2toA_qToRnyqb6Fjbk/edit'
 ];
