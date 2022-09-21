55ce592b5acf461b0e005182efbce74241485ec1
kidcozygr <110769286+kidcozygr@users.noreply.github.com>
2022-09-21 17:37:14 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,10 +8,7 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/1h34V-fRoTsXUDnvJBY3J4khuxGDMfRgpquWZzQpKDNA/edit#responses',
 	'https://docs.google.com/forms/d/130-0pEFDje34CeNuyuUa2xgkP6NbJaVesx_-A3uwW4E/edit',
-	'https://docs.google.com/forms/d/1tJcdrUCZC1Fsovb4Enjz8_HTvtKatuDk-w89bSro2dU/edit',
 	'https://docs.google.com/forms/d/1xhozidzehbZh-JE1RJ938GFxQ3NBKYMVtUJi975iJWs/edit',
-	'https://docs.google.com/forms/d/1SwsJTF7c3SrdURvYUUjvTslapxX1gm0z31Aaz_VPLZg/edit',
-	'https://docs.google.com/forms/d/1TGGM2P8rJJ6r490kFmSkavh1a2toA_qToRnyqb6Fjbk/edit'
+	'https://docs.google.com/forms/d/1SwsJTF7c3SrdURvYUUjvTslapxX1gm0z31Aaz_VPLZg/edit'
 ];
